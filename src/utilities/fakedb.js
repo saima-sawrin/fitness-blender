cf63b8fff0db28d561d87689255b041dd03f9bc9
Saima <saimasawrin324@gmail.com>
2022-09-29 23:24:29 +0600

    storage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -17,7 +17,7 @@ const addToDb = id =>{
     else{
         shoppingCart[id] = 1;
     }
-    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+    localStorage.setItem('time', JSON.stringify(shoppingCart));
 }
 
 const getStoredCart = () =>{
